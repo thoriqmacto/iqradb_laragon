d8235707670f5d0ad859291a860d9edc73322a09
thoriqmacto <thariq.hadad@gmail.com>
2023-12-02 22:48:31 +0700

    Added feature to create news item and finished the tutorial

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,9 @@ use App\Controllers\News;
  */
 $routes->get('/', 'Home::index');
 
-$routes->get('news', [News::class, 'index']);           
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/new', [News::class, 'new']);
+$routes->post('news', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'show']);
 
 $routes->get('pages',[Pages::class,'index']);
